cb16747519a85d548c29e7a9050b79e1171a53f5
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2024-01-12 14:20:54 +0100

    end course of todoList

--- Kurs Mateusz/projekt7/script.js
+++ Kurs Mateusz/projekt7/script.js
@@ -4,21 +4,42 @@ let addBtn // przycisk ADD - dodaje nowe elementy do listy
 let ulList // lista zadań, tagi ul
 let newTodo // nowo dodany LI, nowe zadanie
 
+let popup // popup
+let popupInfo // tekst w popupie, jak się doda pusty tekst
+let todoToEdit // edytowany todo
+let popupInput // input w popupie
+let popupAddBtn // przycisk "zatwierdź" w popupie
+let popupCloseBtn // przycisk "anuluj" w popupie
+
 const main = () => {   
     prepareDOMElements()
     prepareDOMEvents()
 }
 
 const prepareDOMElements = () => {
-    // pobieramy wszystkie elementy
+    // pobieramy wszystkie elementy\
+
+    // Todolist querySelector's
     todoInput = document.querySelector('.todo-input');
     errorInfo = document.querySelector('.error-info');
     addBtn = document.querySelector('.btn-add');
     ulList = document.querySelector('.todolist ul');
+
+    // Popup querySelector's
+    popup = document.querySelector('.popup');
+    popupInfo = document.querySelector('.popup-info');
+    // todoToEdit = document.querySelector('.')
+    popupInput = document.querySelector('.popup-input');
+    popupAddBtn = document.querySelector('.accept');
+    popupCloseBtn = document.querySelector('.cancel');
 }
 
 const prepareDOMEvents = () => {
-    addBtn.addEventListener('click', addNewTodo)
+    addBtn.addEventListener('click', addNewTodo);
+    ulList.addEventListener('click', checkClick);
+    popupCloseBtn.addEventListener('click', closePopup);
+    popupAddBtn.addEventListener('click', changeTodoText);
+    todoInput.addEventListener('keyup', enterKeyCheck);
 }
 
 
@@ -61,13 +82,57 @@ const createToolsArea = () => {
     deleteBtn.classList.add('delete');
     deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
     
-
     toolsPanel.append(completeBtn, editBtn, deleteBtn);
-
 }   
 
+const checkClick = e => {
+    if (e.target.matches('.complete')) {
+        e.target.closest('li').classList.toggle('completed');
+        e.target.classList.toggle('completed');
+    } else if (e.target.matches('.edit')){
+        editTodo(e);
+    } else if (e.target.matches('.delete')){
+        deleteTodo(e);
+    } 
+}
 
 
+const editTodo = e => {
+    todoToEdit = e.target.closest('li');
+    popupInput.value = todoToEdit.firstChild.textContent;
+    popup.style.display = 'flex';
+}
 
+const closePopup = () => { 
+    popup.style.display = 'none';
+    popupInfo.textContent = '';
+}
+
+
+const changeTodoText = () => {
+    if(popupInput.value !== '') {
+        todoToEdit.firstChild.textContent = popupInput.value
+        popup.style.display = 'none';
+        popupInfo.textContent = '';
+    } else {
+        popupInfo.textContent = 'Musisz podać jakąś treść!';
+    }
+}
+
+const deleteTodo = e => { 
+    e.target.closest('li').remove();
+
+    const allTodos = document.querySelectorAll('li');
+
+    if(allTodos.length === 0) {
+        errorInfo.textContent = "Brak zadań na liście!";
+    }
+}
+
+const enterKeyCheck = e => {
+    if(e.key === 'Enter'){
+        addNewTodo()
+    }
+}
 
 document.addEventListener('DOMContentLoaded', main);
